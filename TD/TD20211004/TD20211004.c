df35a2fa448794a9e87f48299d075a58d4c944bc
Carlos Antonio Reyes Peña <carlos.reyespena@heig-vd.ch>
2021-10-07 15:59:30 +0200

    Octobre 7 V1
    Changements

---
+++ TD/TD20211004/TD20211004.c
@@ -0,0 +1,16 @@
+/**
+  - Nom projet : TD
+  - Créateur   Carlos Reyes
+  - Date     04/10/2021
+  - Description creation of c file
+**/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+int main(int argc, const char *argv[])
+{
+    printf("Hello World \n");
+    return 69;
+}
